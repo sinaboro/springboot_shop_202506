295a73406dd5110d666e0417605f13460e3e0ccb
sinaboro <rlaeocjf@gmail.com>
2025-06-20 11:18:00 +0900

    shop project ver 0.4

--- src/main/java/com/example/shop/dto/MemberFormDto.java
+++ src/main/java/com/example/shop/dto/MemberFormDto.java
@@ -1,6 +1,10 @@
 package com.example.shop.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
@@ -8,8 +12,18 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MemberFormDto {
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
     private String name;
+
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "이메일 형식으로 입력해 주세요.")
     private String email;
+
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Length(min=4, max = 8, message = "비밀번호는 4자 이상, 8자 이하로 입력해주세요")
     private String password;
+
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
     private String address;
 }
