2a79408a06e386c0ebc4b519ca763e6a103a7bb3
sinaboro <rlaeocjf@gmail.com>
2025-06-20 10:03:15 +0900

    shop project ver 0.3

---
+++ src/main/java/com/example/shop/service/MemberService.java
@@ -0,0 +1,29 @@
+package com.example.shop.service;
+
+import com.example.shop.entity.Member;
+import com.example.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+@RequiredArgsConstructor
+//@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member save(Member member) {
+       //email 중복 체크
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember != null) {
+            throw new IllegalArgumentException("이미 가입된 회원 입니다.");
+        }
+    }
+}
---
+++ src/test/java/com/example/shop/service/MemberServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.shop.service;
+
+import com.example.shop.dto.MemberFormDto;
+import com.example.shop.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public Member createMember(){
+
+        MemberFormDto memberFormDto = MemberFormDto.builder()
+                .email("test@emai.com")
+                .name("Test")
+                .address("서울시 강동구")
+                .password("1234")
+                .build();
+
+        return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    public void saveMember(){
+        Member member = createMember();
+        Member savedMember = memberService.save(member);
+
+        assertEquals(savedMember.getEmail(), member.getEmail());
+        assertEquals(savedMember.getRole(), member.getRole());
+    }
+}
